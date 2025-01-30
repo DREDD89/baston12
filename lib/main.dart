24c714e816a80f0b578aae0bd71ea8b1032f5953
DREDD89 <andrescruzal03@gmail.com>
2025-01-30 03:09:56 -0600

    3 vuelta

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bastón Inteligente',
+      title: 'Teyankanxool',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6d3acf)),
         useMaterial3: true,
@@ -51,28 +51,86 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF6d3acf),
         title: Text(widget.title),
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.black),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF6d3acf),
+              ),
+              child: Text(
+                'Menú',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person, color: Colors.blue),
+              title: const Text(
+                'Contactar usuario',
+                style: TextStyle(color: Colors.blue),
+              ),
+              onTap: () {
+                // Acción al presionar "Contactar usuario"
+                Navigator.pop(context); // Cerrar el menú
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.alarm, color: Colors.blue),
+              title: const Text(
+                'Hacer sonar el bastón',
+                style: TextStyle(color: Colors.blue),
+              ),
+              onTap: () {
+                // Acción al presionar "Hacer sonar el bastón"
+                Navigator.pop(context); // Cerrar el menú
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.emergency, color: Colors.blue),
+              title: const Text(
+                'Llamar emergencia',
+                style: TextStyle(color: Colors.blue),
+              ),
+              onTap: () {
+                // Acción al presionar "Llamar emergencia"
+                Navigator.pop(context); // Cerrar el menú
+              },
+            ),
+          ],
+        ),
       ),
       body: Stack(
         children: [
           // Imagen de fondo que simula un mapa
           Center(
             child: Image.asset(
-              'assets/mapa_fondo.jpg', // Cambia esto por la ruta de tu imagen
+              'assets/mapa_fondo.jpg',
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
             ),
           ),
 
-          // Botones en la parte inferior
+          // Botones inferiores
           Positioned(
-            bottom: 50, // Margen inferior
-            left: 20, // Margen izquierdo
-            right: 20, // Margen derecho
+            bottom: 50,
+            left: 20,
+            right: 20,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // Botón para hacer sonar el bastón
+                //sonar el bastón
                 GestureDetector(
                   onTap: _toggleBotonBaston,
                   child: Container(
